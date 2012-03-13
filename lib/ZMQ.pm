e4bab16183d188dc0bcf4999af2ea751fdcb6e2b
lestrrat <lestrrat+github@gmail.com>
2012-03-13 09:59:35 +0900

    kinda cherry pick 7cec96fb277db9fd1094c4190d15c006ef4067cc

--- lib/ZMQ.pm
+++ lib/ZMQ.pm
@@ -283,6 +283,26 @@ The callback receives the data received from the socket.
 Register a write callback for a given C<$name>. This is used in C<sendmsg_as()>
 The callback receives the Perl structure given to C<sendmsg_as()>
 
+=head1 DEBUGGING XS
+
+If you see segmentation faults, and such, you need to figure out where the error is occuring in order for the maintainers to figure out what happened. Here's a very very brief explanation of steps involved.
+
+First, make sure to compile ZeroMQ.pm with debugging on by specifying -g:
+
+    perl Makefile.PL -g
+    make
+
+Then fire gdb:
+
+    gdb perl
+    (gdb) R -Mblib /path/to/your/script.pl
+
+When you see the crash, get a backtrace:
+
+    (gdb) bt
+
+Please put this in your bug report.
+
 =head1 CAVEATS
 
 This is an early release. Proceed with caution, please report
